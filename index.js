14b5e5c469aa8c4036960bcb115c8911710618b6
vinodhconnects <vinodhconnects@gmail.com>
2023-05-29 13:00:57 +0530

    js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-images=["first.jpg","second.jpeg","third.jpeg","foruth.jpeg",
+images=["first.jpg","second.jpeg","third.jpeg","fourth.jpeg",
 "fifth.jpeg",
 "sixth.jpg"]
 
+setInterval(
+    function() {
+     document.getElementById("img1").src = images[ Math.round(Math.random()*5)]
+    },2000
+)

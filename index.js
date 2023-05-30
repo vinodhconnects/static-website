c422f721625c5c2bacefa42fccf872e2270e06a6
vinodhconnects <vinodhconnects@gmail.com>
2023-05-30 10:53:22 +0530

    variable-change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+currentIndex=0
 images=["first.jpg","second.jpeg","third.jpeg","fourth.jpeg",
 "fifth.jpeg",
 "sixth.jpg"]
 
 setInterval(
     function() {
+     currentIndex=Math.round(Math.random()*5)
      document.getElementById("img1").src = 
-     images[ Math.round(Math.random()*5)]
+     images[ currentIndex ]
     },10000
 )

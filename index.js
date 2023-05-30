9e984e8da86a733d53abe1e204dfd4707d124bcf
vinodhconnects <vinodhconnects@gmail.com>
2023-05-30 10:58:45 +0530

    arrow-logic

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ setInterval(
      images[ currentIndex ]
     },10000
 )
+
+
+function changeImage(arrowtype){
+    if(arrowtype=="right"){
+       currentIndex = currentIndex==5?0:currentIndex++
+    }
+    else if(arrowtype=="left"){
+        currentIndex = currentIndex==0?5:currentIndex--
+    }
+    document.getElementById("img1").src=images[currentIndex]
+}

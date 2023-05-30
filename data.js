0c24ff4fa74c158946548a428a3917bbb14e97e3
vinodhconnects <vinodhconnects@gmail.com>
2023-05-30 11:23:35 +0530

    data

---
+++ data.js
@@ -0,0 +1,23 @@
+data = {
+    services:[
+    {
+        name: "Application Services",
+        image: "images/application.png",
+        features: ["Latest Technology", "Micro Service Architectiure"
+         ,"Cloud Native","Test Driven Development"]
+    },
+    {
+        name: "Testing Services",
+        image: "images/testing.png",
+        features: ["End to End Testing", "Performance Analysis"
+         ,"Quality Assurance","Powerful tools"]
+    },
+    {
+        name: "Infrastructure Services",
+        image: "images/infrastructure.png",
+        features: ["Cloud Services", "Low Cost"
+         ,"Premium Hardware","High Performance"]
+    }
+
+]
+}

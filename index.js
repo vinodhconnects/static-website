ff49440e26bb4db4edc498a6f54efa8d6a181a9b
vinodhconnects <vinodhconnects@gmail.com>
2023-05-30 11:06:16 +0530

    final-one

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ setInterval(
 
 
 function changeImage(arrowtype){
+    
     if(arrowtype=="right"){
-       currentIndex = currentIndex==5?0:currentIndex++
+       currentIndex = currentIndex==5?0:++currentIndex
     }
     else if(arrowtype=="left"){
-        currentIndex = currentIndex==0?5:currentIndex--
+        currentIndex = currentIndex==0?5:--currentIndex
     }
     document.getElementById("img1").src=images[currentIndex]
 }
